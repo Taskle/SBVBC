5129c66b39d5be198919f4c28e01493df848205d
David Murray <taskle@gmail.com>
2014-05-27 02:40:30 -0700

    require https and ref // instead of http:// on site

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<title>South Bay Volleyball Club</title>
-	<link href='http://fonts.googleapis.com/css?family=Lato|Open+Sans' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Lato|Open+Sans' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 	<link rel="stylesheet" href="/css/main.css">
