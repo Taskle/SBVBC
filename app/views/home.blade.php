daf178ea4ef3153f56dd12299125c64f9ea22c4e
David Murray <taskle@gmail.com>
2014-06-18 22:38:02 -0700

    added team counts to admin screen

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -67,13 +67,13 @@ $(function() {
 @section('content')
 
 @if (Auth::user()->role == 'Admin')
-	<h3>{{ $tournament->name }} <span class="title-detail deemphasis">{{ count($tournament->users) }} players</span></h3>
+	<h3>{{ $tournament->name }} <span class="title-detail deemphasis">{{ count($tournament->teams) }} teams, {{ count($tournament->users) }} players</span></h3>
 	@foreach ($tournament->divisions()->get() as $division)
 		<div class="panel panel-default">
 			<div class="panel-heading">
 				<h3 class="panel-title">
 					<a href="/admin/divisions/{{ $division->id }}">
-						{{ $division->name }} <span class="title-detail deemphasis">{{ count($division->users) }} players</span>
+						{{ $division->name }} <span class="title-detail deemphasis">{{ count($division->teams) }} teams, {{ count($division->users) }} players</span>
 					</a>
 				</h3>
 			</div>
