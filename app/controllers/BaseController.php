27644c7fbde57b1b25d05cff060355ccedcf73be
David Murray <taskle@gmail.com>
2014-12-25 21:00:34 -0800

    now pulling data from stripe pre-filtered by date range related to tournament

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -34,51 +34,63 @@ class BaseController extends Controller {
 
 		// look up payment status for every user
 		Stripe::setApiKey(Config::get('app.stripe.api_key'));
-		$charges = Stripe_Charge::all(array('limit' => 100));
-
-		$prevTournamentDate = 0;
+		
+		// NOTE: we can't get more than 100 charges at a time
+		// so we have to page through them
+		$chargeParams = array('limit' => 100);
+		
+		// if tournament param is provided, only count
+		// charges associated with this tournament, as determined
+		// by the payment date occurred after the prev tourney but 
+		// before this one
 		if ($tournament) {
+			
+			$tourneyDate = strtotime($tournament->date);
+			if ($tourneyDate < time()) {
+				$chargeParams['created[lte]'] = $tourneyDate;
+			}
+			
 			$prevTournament = $tournament->getPreviousTournament();
 			if ($prevTournament) {
-				$prevTournamentDate = strtotime($prevTournament->date);
+				$chargeParams['created[gte]'] = strtotime($prevTournament->date);
 			}
 		}
+		
+		// get charges from stripe based on params above (note:
+		// if no param provided, all charges are pulled, capped at the limit
+		// provided
+		$charges = Stripe_Charge::all($chargeParams);
 
 		$payments = array();
 		$numCharges = count($charges->data);
-		for ($i = 0; $i < $numCharges; $i++) {
+		
+		// go through all pages of data from stripe
+		while ($numCharges > 0) {
+			
+			$charge = null;
+			for ($i = 0; $i < $numCharges; $i++) {
 
-			$charge = $charges->data[$i];
+				$charge = $charges->data[$i];
 
-			// note: $charge->card->name is actually the person's email address
-			if ($charge->paid && $charge->card->name) {
+				// note: $charge->card->name is actually the person's email address
+				if ($charge->paid && $charge->card->name) {
 
-				// if tournament param is provided, only count
-				// charges associated with this tournament, as determined
-				// by the payment date occurred after the prev tourney but 
-				// before this one
-				if ($tournament) {
+					// get total minus refund
+					$amount = $charge->amount - $charge->amount_refunded;
 
-					// if name not in description and charge was not between
-					// last tourney and this one
-					if ($charge->created < $prevTournamentDate ||
-						$charge->created > strtotime($tournament->date)) {
-
-						// this charge isn't for this tournament, so ignore it
-						continue;
+					if (!isset($payments[$charge->card->name])) {
+						$payments[$charge->card->name] = 0;
 					}
+
+					$payments[$charge->card->name] +=
+							($amount / 100.0);
 				}
-
-				// get total minus refund
-				$amount = $charge->amount - $charge->amount_refunded;
-
-				if (!isset($payments[$charge->card->name])) {
-					$payments[$charge->card->name] = 0;
-				}
-
-				$payments[$charge->card->name] +=
-						($amount / 100.0);
 			}
+			
+			// go through next set of charges
+			$chargeParams['starting_after'] = $charge->id;
+			$charges = Stripe_Charge::all($chargeParams);
+			$numCharges = count($charges->data);
 		}
 
 		return $payments;
